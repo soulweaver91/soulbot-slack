b5f2f43871c357b9b7ecf48e067b90e2f2a816cf
Soulweaver <soulweaver@hotmail.fi>
2016-10-14 03:09:38 +0300

    Add command for throwing a die

--- plugins/choices.py
+++ plugins/choices.py
@@ -7,7 +7,7 @@ hazy_replies    = [line.rstrip('\r\n') for line in open('data/choices/hazies.txt
 helpful_ratio   = 0.95
 
 
-def process_message(data):
+def random_choice(data):
     """
     Chooses a random item from a slash-delimitered list.
 
@@ -15,21 +15,56 @@ def process_message(data):
     :return: None
     """
 
-    if data["text"][:7] in ['!choose', '!choice']:
-        items = list(filter(None, [s.strip() for s in data["text"][7:].split('/')]))
+    items = list(filter(None, [s.strip() for s in data["text"][7:].split('/')]))
 
-        if len(items) > 1 and random.random() > helpful_ratio:
-            reply = random.choice(hazy_replies)
-        else:
-            reply = random.choice(helpful_replies)
+    if len(items) > 1 and random.random() > helpful_ratio:
+        reply = random.choice(hazy_replies)
+    else:
+        reply = random.choice(helpful_replies)
 
-        if len(items) > 1:
-            return outputs.append([data["channel"], reply.format(random.choice(items))])
-        elif len(items) == 1:
-            return outputs.append([data["channel"], reply.format('Try more options next time')])
-        else:
-            return outputs.append([data["channel"], reply.format('Give me something to choose from first')])
+    if len(items) > 1:
+        return outputs.append([data["channel"], reply.format(random.choice(items))])
+    elif len(items) == 1:
+        return outputs.append([data["channel"], reply.format('Try more options next time')])
+    else:
+        return outputs.append([data["channel"], reply.format('Give me something to choose from first')])
+
+
+def throw_dice(data):
+    """
+    Throws an n-sided die.
+
+    :param data: RTM message
+    :return: None
+    """
+
+    args = data["text"].split()[1:]
+
+    if len(args) > 0:
+        try:
+            sides = int(args[0])
+        except (TypeError, ValueError):
+            return outputs.append([data["channel"], "That doesn't seem like a number of sides."])
+    else:
+        sides = 6
+
+    if sides <= 0:
+        return outputs.append([data["channel"], "Please provide a positive number of sides."])
+
+    return outputs.append([data["channel"], "You rolled a {}!".format(random.randint(1, sides))])
+
+
+def process_message(data):
+    cmd = data["text"].split()[0]
+
+    if cmd in ['!choose', '!choice']:
+        return random_choice(data)
+    elif cmd in ['!die', '!dice']:
+        return throw_dice(data)
 
 
 def get_module_help():
-    return '`!choice` or `!choose`: Pick a random item from a slash-delimitered list.'
+    return '\n'.join([
+        '`!choice` or `!choose`: Pick a random item from a slash-delimitered list.',
+        '`!die` or `!dice`: Throw a die. Optionally, provide the number of sides: `!die 20`'
+    ])
